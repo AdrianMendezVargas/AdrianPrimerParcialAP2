d4a9e7ff1e01f91132b9cbdb9aba86c42c5e1506
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-10-07 18:23:03 -0400

    Agregando contexto y paquetes

---
+++ AdrianPrimerParcialAP2/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using AdrianPrimerParcialAP2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdrianPrimerParcialAP2.DAL {
+    public class Contexto : DbContext{
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite("Data Source= Data\\Registro.db");
+        }
+
+    }
+}
